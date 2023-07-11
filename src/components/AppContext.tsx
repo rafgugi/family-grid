2bf52bf0611bba64041a518a0ca7a1ae7f2dc0ad
risyanggi.faizin <risyanggi.faizin@gojek.com>
2023-07-11 10:49:20 +0700

    feat: add Tree via modal

--- src/components/AppContext.tsx
+++ src/components/AppContext.tsx
@@ -5,6 +5,7 @@ interface AppContextValue {
   split: boolean;
   editMode: boolean;
   hidePersonCode: boolean;
+  addTree: (t: Person) => void;
   setTreeValue: (p: Person) => void;
   deleteTreePerson: (p: Person) => void;
   treeMap: Record<string, Person>;
@@ -14,6 +15,7 @@ const AppContext = createContext<AppContextValue>({
   split: false,
   editMode: false,
   hidePersonCode: false,
+  addTree: () => {},
   setTreeValue: () => {},
   deleteTreePerson: () => {},
   treeMap: {},
---
+++ src/components/ModalAddTree.tsx
@@ -0,0 +1,78 @@
+import {
+  Button,
+  FormFeedback,
+  FormGroup,
+  Input,
+  Label,
+  Modal,
+  ModalBody,
+  ModalFooter,
+  ModalHeader,
+} from 'reactstrap';
+import { useContext, useState } from 'react';
+import { Marriage, Person } from '../family.interface';
+import AppContext from './AppContext';
+
+interface ModalAddTreeProps {
+  isOpen: boolean;
+  toggle: () => void;
+}
+
+function ModalAddTree({ isOpen, toggle }: ModalAddTreeProps) {
+  const { treeMap, addTree } = useContext(AppContext);
+  const [child, setChild] = useState('');
+  const [childError, setChildError] = useState('');
+
+  const handleChildChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+    setChild(value);
+
+    setChildError('');
+    if (Object.keys(treeMap).includes(value)) {
+      setChildError(value + ' is already taken');
+    }
+  };
+
+  const validForm = () => child && !childError;
+
+  const handleSubmit = () => {
+    if (!validForm()) return;
+
+    const tree: Person = {
+      id: child,
+      code: '',
+      marriages: [] as Marriage[],
+    };
+
+    addTree(tree);
+    setChild('');
+    toggle();
+  };
+
+  return (
+    <Modal isOpen={isOpen} toggle={toggle} unmountOnClose>
+      <ModalHeader toggle={toggle}>Add a tree</ModalHeader>
+      <ModalBody>
+        <FormGroup>
+          <Label for="input-child">Child</Label>
+          <Input
+            id="input-tree"
+            type="text"
+            placeholder="Insert tree nickname"
+            value={child}
+            onChange={handleChildChange}
+            invalid={childError !== ''}
+          />
+          {childError !== '' && <FormFeedback>{childError}</FormFeedback>}
+        </FormGroup>
+      </ModalBody>
+      <ModalFooter>
+        <Button disabled={!validForm()} onClick={handleSubmit}>
+          Submit
+        </Button>
+      </ModalFooter>
+    </Modal>
+  );
+}
+
+export default ModalAddTree;
