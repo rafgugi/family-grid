b87d11e34188e918b567dc5d1cf18c769d857d15
risyanggi.faizin <risyanggi.faizin@gojek.com>
2024-04-15 09:53:03 +0700

    chore: add id translation

--- src/i18n.tsx
+++ src/i18n.tsx
@@ -1,10 +1,11 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import en from './lang-en.yml';
+import id from './lang-id.yml';
 
 i18n.use(initReactI18next).init({
   lng: 'en',
-  resources: { en },
+  resources: { en, id },
   interpolation: {
     escapeValue: false,
   },
