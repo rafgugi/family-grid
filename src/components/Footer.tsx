073829a51ab75422d3f50bbd1f9ba5c006750e24
Risyanggi Azmi Faizin <risyanggi@gmail.com>
2026-03-12 10:32:16 +0700

    refactor: update environment variables to Vite format

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import { Container } from 'reactstrap';
 
 const Footer = () => {
   const repo_url = 'https://github.com/rafgugi/family-grid';
-  const version = process.env.REACT_APP_VERSION as string;
+  const version = import.meta.env.VITE_VERSION as string;
 
   return (
     <footer className="d-print-none bg-dark text-light py-3 mt-5">
